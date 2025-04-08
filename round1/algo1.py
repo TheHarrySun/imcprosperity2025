933d200dc36b0327a9f1d26ae8a2f65b17bdc79c
Harrison Xu <hsx925@gmail.com>
2025-04-08 02:19:10 -0400

    did some investigation on linreg

--- round1/algo1.py
+++ round1/algo1.py
@@ -2,6 +2,9 @@ from datamodel import OrderDepth, Order, UserId, TradingState
 from typing import List
 import string
 
+class ResinTrade:
+    
+
 class Trader:
     def run(self, state: TradingState):
         pass
