c1b61f9e342475a6131248961be8913b9b78fe44
Harrison Xu <hsx925@gmail.com>
2025-04-08 02:42:07 -0400

    starting algo

--- round1/algo1.py
+++ round1/algo1.py
@@ -2,8 +2,11 @@ from datamodel import OrderDepth, Order, UserId, TradingState
 from typing import List
 import string
 
-class ResinTrade:
-    
+
+class Product:
+    RESIN = "RAINFOREST_RESIN"
+    INK = "SQUID_INK"
+    KELP = "KELP"
 
 class Trader:
     def run(self, state: TradingState):
