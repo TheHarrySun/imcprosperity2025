7ca12f806c0ea2ac38d16c9c0237655f57d29e58
Harrison Xu <hsx925@gmail.com>
2025-04-09 02:31:51 -0400

    take_best_orders

--- round1/algo1.py
+++ round1/algo1.py
@@ -1,5 +1,5 @@
 from datamodel import OrderDepth, Order, UserId, TradingState
-from typing import List
+from typing import List, Tuple
 import string
 
 
@@ -17,9 +17,47 @@ class Trader:
             Product.KELP: 50
         }
         
-    def take_best_orders(self, product: str, fair_value: int, take_width: float, orders: List[Order], order_depth: OrderDepth):
+    def take_best_orders(
+        self, 
+        product: str, 
+        fair_value: int, 
+        take_width: float, 
+        orders: List[Order], 
+        order_depth: OrderDepth,
+        position: int,
+        buy_order_volume: int,
+        sell_order_volume: int
+    ) -> Tuple[int, int]:
         prod_limit = self.LIMIT[product]
         
+        if len(order_depth.sell_orders) != 0:
+            best_ask = min(order_depth.sell_orders.keys())
+            best_ask_amt = -1 * order_depth.sell_orders[best_ask]
+            
+            if (best_ask <= fair_value - take_width):
+                quantity = min(best_ask_amt, prod_limit - position)
+                if quantity > 0:
+                    orders.append(Order(product, best_ask, quantity))
+                    buy_order_volume += quantity
+                    order_depth.sell_orders[best_ask] += quantity
+                    if order_depth.sell_orders[best_ask] == 0:
+                        del order_depth.sell_orders[best_ask]
+            
+        if len(order_depth.buy_orders) != 0:
+            best_bid = max(order_depth.buy_orders.keys())
+            best_bid_amt = order_depth.buy_orders[best_bid]
+            
+            if best_bid >= fair_value + take_width:
+                quantity = min(best_bid_amt, prod_limit + position)
+                
+                if quantity > 0:
+                    orders.append(Order(product, best_bid, -1 * quantity))
+                    sell_order_volume += quantity
+                    order_depth.buy_orders[best_bid] -= quantity
+                    if order_depth.buy_orders[best_bid] == 0:
+                        del order_depth.buy_orders[best_bid]
+        
+        return buy_order_volume, sell_order_volume
         
     def run(self, state: TradingState):
         pass
